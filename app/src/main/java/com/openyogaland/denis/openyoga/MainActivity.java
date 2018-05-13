8c039813cd0bb1f19828eaffc272f97106d55122
castasat <castasat@mail.ru>
2018-05-13 22:26:20 +0300

    - overriden OnCreate method

--- app/src/main/java/com/openyogaland/denis/openyoga/MainActivity.java
+++ app/src/main/java/com/openyogaland/denis/openyoga/MainActivity.java
@@ -1,9 +1,18 @@
 package com.openyogaland.denis.openyoga;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity
 {
-  // обновим содержимое
-  // выполним из меню VCS > Commit > Commit and Push > Push
+  /**
+   * This method is called then Activity is created
+   * @param savedInstanceState - this is a saved activity instance state
+   */
+  @Override
+  protected void onCreate(@Nullable Bundle savedInstanceState)
+  {
+    super.onCreate(savedInstanceState);
+  }
 }
