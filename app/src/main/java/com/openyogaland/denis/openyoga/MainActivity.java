bdeb882a510f0ac605bbcc32f0dbe0e62863f10c
castasat <castasat@mail.ru>
2018-05-13 22:10:11 +0300

    - удаленная часть файла показана серым - добавленная новая часть - зелёным

--- app/src/main/java/com/openyogaland/denis/openyoga/MainActivity.java
+++ app/src/main/java/com/openyogaland/denis/openyoga/MainActivity.java
@@ -4,10 +4,6 @@ import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity
 {
-  // сейчас мы разместим наш проект в репозитории github
-  // в меню выбираем VCS > Import into version control > Share project on GitHub
-  // я зашел в свой аккаунт на гитхабе https://github.com/castasat
-  // сейчас там 9 репозиториев - выполняем команду
-  // ошибка в описании репозитория
-  
+  // обновим содержимое
+  // выполним из меню VCS > Commit > Commit and Push > Push
 }
